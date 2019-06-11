fd74217ecd352f651f96b42ac25eb530fad4126a
Ethan Brierley <ethanboxx@gmail.com>
2019-06-11 14:27:34 +0100

    remove imports

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,15 +4,5 @@ module Lib
     )
 where
 
-import           Data.Text.Lazy                as T
-import           Yaml
-import           Data.Yaml                     as Y
-import           GHC.Generics
-import           Data.ByteString               as BS
-import           Data.Text.Lazy.Encoding       as TLE
-import           Prelude                       as P
-import           Data.Yaml.Builder
-import           System.IO                     as S
 import           Parse
-
 
