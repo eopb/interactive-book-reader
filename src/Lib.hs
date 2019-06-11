0580c4b840389704b96695e434adfcd4ce0b056b
Ethan Brierley <ethanboxx@gmail.com>
2019-06-11 10:16:28 +0100

    yaml test

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,5 +3,29 @@ module Lib
     )
 where
 
+
+import           Data.Text                     as T
+
 mainTask :: IO ()
-mainTask = print "test"
+mainTask = putStrLnT yaml
+
+
+
+
+
+yaml :: T.Text
+yaml = multiLineText
+    [ "book:"
+    , "  -"
+    , "    chapter: 1"
+    , "    content: >"
+    , "      Welcome to the test book for this program"
+    , "      You have 3 options."
+    ]
+
+multiLineText :: [T.Text] -> T.Text
+multiLineText t = T.concat (fmap (\x -> append x "\n") t)
+
+
+putStrLnT :: T.Text -> IO ()
+putStrLnT = putStrLn . T.unpack
